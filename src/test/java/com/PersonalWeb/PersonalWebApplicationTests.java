694962a56cf97612e7d88095d7ca89b6c4de3752
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2023-11-05 17:49:27 +0800

    Initial commit

---
+++ src/test/java/com/PersonalWeb/PersonalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PersonalWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
