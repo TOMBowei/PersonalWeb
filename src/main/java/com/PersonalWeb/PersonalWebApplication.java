1381e153e8b1fe58c0298f081945e3793873edbd
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2023-11-07 14:39:57 +0800

    add more

--- src/main/java/com/PersonalWeb/PersonalWebApplication.java
+++ src/main/java/com/PersonalWeb/PersonalWebApplication.java
@@ -1,9 +1,11 @@
 package com.PersonalWeb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.PersonalWeb.mapper")
 public class PersonalWebApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/PersonalWeb/controller/IndexController.java
+++ src/main/java/com/PersonalWeb/controller/IndexController.java
@@ -1,14 +1,24 @@
 package com.PersonalWeb.controller;
 
+import com.PersonalWeb.entity.User;
+import com.PersonalWeb.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private IUserService userService;
+
     @GetMapping("/index")
     @ResponseBody
     public String index() {
+        for (User user : userService.list()) {
+            System.out.println(user.getUserName());
+        }
         return "Hello World!";
     }
 }
